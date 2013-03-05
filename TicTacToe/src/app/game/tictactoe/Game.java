e88febf9b1960112d716c649e40d9f1820e0f5c0
donotask <rockblack.tw@yahoo.com.tw>
2013-03-04 23:01:11 -0800

    new Game.java

--- TicTacToe/src/app/game/tictactoe/Game.java
+++ TicTacToe/src/app/game/tictactoe/Game.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 
 public class Game {
 	
-	// 1 represent circle, 2 represents cross
-	
+	// player 1 represents circle, player 2 represents cross
+	protected int player = 1;
 	protected boolean gameEnd = false;
 	protected boolean isTie = false;
 	
 	protected int[][] map = null;
+	protected int spaces = 9;
 	
 	public void init() {
 		map = new int[3][3];
 	}
 	
 	public void start() {
-		Scanner reader = new Scanner(System.in);
 		//get user input for a
 		int player = 1;
 		int x = 0;
@@ -27,64 +27,44 @@ public class Game {
 			System.out.println("Player" + player);
 			showMap();
 			System.out.println("Enter x, please pick a number between 1 and 3. (-1 to exit)");
-			x = reader.nextInt();
-			if (x == -1) {
-				break;
-			}
 			System.out.println("Enter y, please pick a number between 1 and 3. (-1 to exit)");
-			y = reader.nextInt();
-			if (y == -1) {
-				break;
-			}
-			if (!mark(player, x, y)) {
+			if (!mark(x, y)) {
 				System.out.println("The spot you picked has already been taken. Please pick another one.");
 				continue;
 			}
-			if (player == 1) {
-				player = 2;
-			} else {
-				player = 1;
-			}
-		}
-		reader.close();
-		if (x == -1 || y == -1) {
-			System.out.println("Game was aborted...");
-			return;
 		}
 		if (isTie) {
 			System.out.println("Nobody is the winner, it's a tie.");
 			return;
 		}
-		showMap();
-		if (player == 1) {
-			System.out.println("Player2 Won!");
-		} else {
-			System.out.println("Player1 Won!");
-		}
 	}
 	
-	public boolean mark(int player, int x, int y) {
-		x = x-1;
-		y = y-1;
+	public boolean mark(int x, int y) {
 		if (map[x][y] != 0) {
 			return false;
 		}
-		setMap(player, x, y);
-		if (checkTie()) {
+		map[x][y] = player;
+		spaces--;
+		gameEnd = checkGame();
+		if (spaces == 0) {
 			gameEnd = true;
+			isTie = true;
+			return true;
 		}
-		if(checkGame(player)){
-			gameEnd = true;
+		if (gameEnd) {
+			return true;
+		}
+		
+		if (player == 1) {
+			player = 2;
+		} else {
+			player = 1;
 		}
 		return true;
 	}
 	
-	protected void setMap(int player, int x, int y) {
-		map[x][y] = player;
-	}
-	
 	// Could still work on more efficient way to checkGame
-	public boolean checkGame(int player) {
+	public boolean checkGame() {
 		for (int i = 0; i < 3; i++) {
 			if (map[i][0] == player && map[i][1] == player && map[i][2] == player) {
 				return true;
@@ -104,26 +84,7 @@ public class Game {
 		return false;
 	}
 	
-	public boolean checkTie() {
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (map[i][j] == 0) {
-					return false;
-				}
-			}
-		}
-		isTie = true;
-		return true;
-	}
-	
 	public void showMap(){
-//		for (int i = 0; i < 3; i++) {
-//			for (int j = 0; j < 3; j++) {
-//				System.out.print(map[i][j]);
-//				System.out.print("|");
-//			}
-//			System.out.println("---------");
-//		}
 		System.out.println(map[0][0] + "|" + map[1][0] + "|" + map[2][0]);
 		System.out.println("-----");
 		System.out.println(map[0][1] + "|" + map[1][1] + "|" + map[2][1]);
@@ -135,9 +96,7 @@ public class Game {
 		return map;
 	}
 	
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.init();
-		game.start();
+	public int getPlayer() {
+		return player;
 	}
 }
