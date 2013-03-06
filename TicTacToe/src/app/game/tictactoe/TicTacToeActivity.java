9ff37ec3b4e4b73fc3eda732ff427adda8f848b4
Lisa <lisaqu87@gmail.com>
2013-03-05 21:46:04 -0800

    disable all buttons when game ended

--- TicTacToe/src/app/game/tictactoe/TicTacToeActivity.java
+++ TicTacToe/src/app/game/tictactoe/TicTacToeActivity.java
@@ -1,21 +1,46 @@
 package app.game.tictactoe;
 
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class TicTacToeActivity extends Activity {
 	private Button turnBtn = null;
+	private ArrayList<Button> buttons = new ArrayList<Button>();
 	private Game game = new Game();
+	
+	private void getAllButtons() {
+		TableLayout view = (TableLayout) this.findViewById(R.id.game_board);
+		int childcount = view.getChildCount();
+		
+		for (int i = 0; i < childcount; i++){
+		      TableRow tr = (TableRow) view.getChildAt(i);
+		      for (int j = 0; j < tr.getChildCount(); j++){
+		          buttons.add(((Button) tr.getChildAt(j)));
+		      }
+		}	
+	}
+	
+	private void disableButtons() {
+		for (Button b : buttons) {
+			b.setEnabled(false);
+		}
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tic_tac_toe);
-		game.init();	
+		
+		game.init();
+		getAllButtons();	
 		turnBtn = (Button) this.findViewById(R.id.player);
 		turnBtn.setText("Player 1's turn");
 	}
@@ -30,21 +55,20 @@ public class TicTacToeActivity extends Activity {
     public void setText(View view) {
     	Button b = (Button) view;
     	String[] tag = b.getTag().toString().split("_");
-    	int x = Integer.parseInt(tag[0]);
-    	int y = Integer.parseInt(tag[1]);
     	int currentPlayer = game.getPlayer();
     	
     	b.setEnabled(false);
         b.setText(currentPlayer == 1 ? "x" : "O");
-        game.mark(x,y);
+        game.mark(Integer.parseInt(tag[0]),Integer.parseInt(tag[1]));
         
         if (game.getGameStatus() == 1) {
             turnBtn.setText("Player " + currentPlayer + "won!");
+            disableButtons();
         } else if (game.getGameStatus() == 0) {
             turnBtn.setText("Game Ended with a tie");
+            disableButtons();
         } else {
             turnBtn.setText("Player " +  game.getPlayer() + "'s turn");
         }
-    }
-    
+    }   
 }
