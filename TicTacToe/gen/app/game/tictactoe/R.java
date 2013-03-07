511aa336c6fc358991183360f3d95725ddd4e2b2
Lisa <lisaqu87@gmail.com>
2013-03-06 22:01:44 -0800

    ui

--- TicTacToe/gen/app/game/tictactoe/R.java
+++ TicTacToe/gen/app/game/tictactoe/R.java
@@ -13,7 +13,7 @@ public final class R {
     public static final class color {
         public static final int blue=0x7f040002;
         public static final int green=0x7f040001;
-        public static final int red=0x7f040000;
+        public static final int white=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -26,28 +26,29 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int rounded_corner=0x7f020001;
+        public static final int button_state=0x7f020000;
+        public static final int button_text_state=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int main_bg=0x7f020003;
     }
     public static final class id {
-        public static final int LinearLayout02=0x7f090000;
-        public static final int action_settings=0x7f090011;
-        public static final int button_0=0x7f090005;
-        public static final int button_1=0x7f090006;
-        public static final int button_2=0x7f090007;
-        public static final int button_3=0x7f090009;
-        public static final int button_4=0x7f09000a;
-        public static final int button_5=0x7f09000b;
-        public static final int button_6=0x7f09000d;
-        public static final int button_7=0x7f09000e;
-        public static final int button_8=0x7f09000f;
-        public static final int game_board=0x7f090003;
-        public static final int p1=0x7f090001;
-        public static final int p2=0x7f090002;
-        public static final int player=0x7f090010;
-        public static final int tableRow1=0x7f090004;
-        public static final int tableRow2=0x7f090008;
-        public static final int tableRow3=0x7f09000c;
+        public static final int action_settings=0x7f090010;
+        public static final int button_0=0x7f090004;
+        public static final int button_1=0x7f090005;
+        public static final int button_2=0x7f090006;
+        public static final int button_3=0x7f090008;
+        public static final int button_4=0x7f090009;
+        public static final int button_5=0x7f09000a;
+        public static final int button_6=0x7f09000c;
+        public static final int button_7=0x7f09000d;
+        public static final int button_8=0x7f09000e;
+        public static final int game_board=0x7f090002;
+        public static final int p1=0x7f090000;
+        public static final int p2=0x7f090001;
+        public static final int player=0x7f09000f;
+        public static final int tableRow1=0x7f090003;
+        public static final int tableRow2=0x7f090007;
+        public static final int tableRow3=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -91,5 +92,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int home_page_btn=0x7f070002;
     }
 }
